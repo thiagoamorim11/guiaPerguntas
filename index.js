cff0e23eda3c626d1700ae90a2e3f80468648cb5
fluxo dolar <fluxodolar2021@gmail.com>
2021-11-28 00:15:46 -0300

    envio de formulario, pelo method POST e instacao da biblioteca body parser

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express  = require("express");
 const app = express();
-
+const bodyParser = require("body-parser")
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//rotas
 app.get("/",(req, res) => {
 
     res.render("index");
@@ -13,6 +17,12 @@ app.get("/",(req, res) => {
 
 app.get("/perguntar",(req, res) =>{
 res.render("perguntar");
+});
+
+app.post("/salvarpergunta",(req,res) =>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+res.send("Formulário recebido! Titulo: " + titulo + "" + " descricao: " + descricao);
 });
 
 app. listen(8080,()=>{
